3bd334c44daab938054e749b6fff1adf83c68337
Etienne <etienne.bertin@imt-atlantique.net>
2025-11-27 10:16:45 +0100

    deleted useless conversion scripts

--- scripts/convert_keras_model_to_tflite.py
+++
@@ -1,8 +0,0 @@
-# Sur votre PC, avec tensorflow complet installé
-import tensorflow as tf
-model = tf.keras.models.load_model("models/modelCNN.keras")
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-# Ajoutez des optimisations si nécessaire
-tflite_model = converter.convert()
-with open("models/modelCNN.tflite", "wb") as f:
-    f.write(tflite_model)
--- scripts/convert_ultralytics_to_tflite.py
+++
@@ -1,4 +0,0 @@
-# Sur votre PC, avec ultralytics installé
-from ultralytics import YOLO
-model = YOLO("models/model.pt")
-model.export(format="tflite")  # Cela créera un model_float32.tflite
